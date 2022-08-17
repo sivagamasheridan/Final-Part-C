f24f2d8895205c408168bf752bc0964fd5b73c1a
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2022-08-17 16:57:11 -0400

    check char

---
+++ src/checkstring/CheckString.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkstring;
+
+/** This class to checks the given string has any repeated character or not.
+ * Complete this method and test this method in JUnit.
+ * @author srinivsi August 
+  */
+public class CheckString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+    String c="examm";
+    if(checkchar(c))
+    {
+        System.out.println("Given String has repeated character ");
+    }
+    else
+    {
+    System.out.println("Given String has not repeated character ");
+    }
+    }     
+
+
+//create method here to check the given String has any repeated character  or not
+}
